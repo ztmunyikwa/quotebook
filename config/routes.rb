2b13a4c59dadb707ea81a9063acf7ff4b38f1d48
Zanele Munyikwa <ztm3@duke.edu>
2013-11-19 03:52:23 +0000

    Added static about me page and Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Quotebook::Application.routes.draw do
+  get "pages/info"
+
+  root :to => redirect('/ideas')
+  resources :ideas
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
